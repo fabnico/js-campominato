5995cc96898e12c54e1f4f5af1358394d22d1a8d
Fabrizio <whoet.wo@gmail.com>
2020-10-29 10:25:04 +0100

    FIX addList function

--- js/script.js
+++ js/script.js
@@ -1,50 +1,61 @@
 var MAX_TRY = 2;
 var WIN_COUNTER = 0;
+
 var game = document.getElementById('gameHTML');
 var result = document.getElementById('resultHTML');
-var gameState = false;
+
+var gameStatus = false;
+var victory = false;
+var pcNumList = []; //array in cui inserire i numeri generati random
+var userNumberList = [];  //array in cui inserire i numeri dell'utente
 
 function randomNum(min, max){
    return Math.floor(Math.random() * (max - min +1) + min );
 }
 
-function addUserList(number){ // Aggiunge il numero dell'utente ad un array
-   return userNumberList.push(number);
+function addUserList(number, array){
+   return array.push(number);
 }
 
-var pcNumList = []; //array in cui inserire i numeri generati random
-
-for (var i = 1; i <= 16; i++){  //sto impiegando troppo tempo per inserire il controllo sull'unicità dei numeri, continuo e ci ripenso dopo.
-      pcNumList.push(randomNum(1,100));
+while(pcNumList.length < 16){ // Aggiunge i numeri random non doppioni ad un array
+   var r = randomNum(1,16)
+   if (!pcNumList.includes(r)){
+      pcNumList.push(r)
    }
+}
 console.log(pcNumList);
 
-var userNumberList = [];  //array in cui inserire i numeri dell'utente
 
 for (var i = 1; i <= MAX_TRY; i++){
    WIN_COUNTER++;
    var userNumber = parseInt(prompt("Inserisci un numero"));
    if(userNumberList.includes(userNumber)){
-      console.log("Hai già inserito questo numero, prova con un altro")
+      alert("Hai già inserito questo numero")
       i--;
       WIN_COUNTER--;
    }else{
-      addUserList(userNumber)
+      addUserList(userNumber, userNumberList)
    }
    console.log(userNumberList)
    for (var j = 0; j < pcNumList.length; j++){
       if(pcNumList[j] == userNumber){
          i = MAX_TRY;
          WIN_COUNTER--;
-         var gameState = true;
+         var gameStatus = true;
       }
       if(i == MAX_TRY){
-         var gameState = true;
+         var victory = true;
       }
    }
 }
 
-if (gameState == true){
+if (gameStatus == true){
    game.innerHTML = "Game over!";
    result.innerHTML = "Punteggio: " + WIN_COUNTER;
+}
+
+if (victory == true && gameStatus == false){
+   game.innerHTML = "Complimenti! Hai vinto.";
+   result.innerHTML = "Punteggio: " + WIN_COUNTER;
+
 }
